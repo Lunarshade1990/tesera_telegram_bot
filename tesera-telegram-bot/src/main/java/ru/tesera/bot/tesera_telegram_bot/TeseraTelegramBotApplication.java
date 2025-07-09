e5023a4fd14c95a30fce79a2528b414958a4ea42
Куксова Инга-Виктория <na.yada1990@gmail.com>
2025-07-09 22:46:05 +0300

    Initial Spring Boot Telegram bot project

---
+++ tesera-telegram-bot/src/main/java/ru/tesera/bot/tesera_telegram_bot/TeseraTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.tesera.bot.tesera_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeseraTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeseraTelegramBotApplication.class, args);
+	}
+
+}
---
+++ tesera-telegram-bot/src/test/java/ru/tesera/bot/tesera_telegram_bot/TeseraTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tesera.bot.tesera_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeseraTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
